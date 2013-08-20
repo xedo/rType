bdd165b48a57c53b0db0aa44a5e169421db82b30
Echedey Lima <echedeylima@gmail.com>
2013-08-20 13:22:20 +0200

    starting animations

---
+++ RType/src/com/echedey/rtype/Animation.java
@@ -0,0 +1,37 @@
+package com.echedey.rtype;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Animation extends JPanel implements ActionListener{
+	
+	Image img;
+	Timer time;
+	int x, y; //position
+	
+	public Animation(String level){
+		setBackground(Color.BLACK);
+		setDoubleBuffered(true);
+		time = new Timer(30,this);
+		time.start(); //start actionPerfomred
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.dispose();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		
+	}
+
+}
