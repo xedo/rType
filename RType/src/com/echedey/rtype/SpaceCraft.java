ec9bbecda52182fe8e224851d2a1b4b9c2fc6620
Echedey Lima <echedeylima@gmail.com>
2013-08-20 14:17:56 +0200

    keys to move

--- RType/src/com/echedey/rtype/SpaceCraft.java
+++ RType/src/com/echedey/rtype/SpaceCraft.java
@@ -1,11 +1,63 @@
 package com.echedey.rtype;
 
+import java.awt.event.KeyEvent;
+
 public class SpaceCraft extends Craft {
-	private String imgURL = "../craft.png";
+	private String imgURL = "../spaceCraft.png";
 	
 	public SpaceCraft (){
 		super();
 		
-		
 	}
+	
+	public void pressKey (KeyEvent k){
+		int key = k.getKeyCode();
+		
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			setMovX(-1);
+			break;
+			
+		case KeyEvent.VK_RIGHT:
+			setMovX(1);
+			break;
+			
+		case KeyEvent.VK_UP:
+			setMovY(-1);
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			setMovY(1);
+			break;
+			
+		default:
+			break;
+		}
+	}
+	
+	public void releaseKey (KeyEvent k){
+		int key = k.getKeyCode();
+		
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			setMovX(0);
+			break;
+			
+		case KeyEvent.VK_RIGHT:
+			setMovX(0);
+			break;
+			
+		case KeyEvent.VK_UP:
+			setMovY(0);
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			setMovY(0);
+			break;
+			
+		default:
+			break;
+		}
+	}
+
 }
