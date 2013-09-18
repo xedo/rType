631b91b6167afd15c4c8708d7ea55de3e1a83af9
Echedey Lima <echedeylima@gmail.com>
2013-09-18 11:16:20 +0200

    limit space Y

--- RType/src/com/echedey/rtype/Enemies.java
+++ RType/src/com/echedey/rtype/Enemies.java
@@ -12,10 +12,17 @@ public class Enemies extends Craft {
 		setImg(imgIcon.getImage().getScaledInstance(50, 50,
 				java.awt.Image.SCALE_SMOOTH));
 
+		// limit space
+
+		// wait for load the img (scale is a bit slow)
+		while ((getImg().getHeight(null) == -1))
+			continue;
+		setMaxY(getMaxY() - getImg().getHeight(null)); // - height image
+
 		setSpeed(5);
 		setMovX(-1);
 		setMovY(0);
-		//set random horizontal positions
+		// set random horizontal positions
 		setX(getMaxX() + (int) (Math.random() * 800));
 		setY((int) (Math.random() * 600));
 
@@ -25,8 +32,8 @@ public class Enemies extends Craft {
 	public void move() {
 		// X
 		setX(getX() + getMovX() * getSpeed());
-//		if (getX() >= getMaxX())
-//			setX(getMinX());
+		// if (getX() >= getMaxX())
+		// setX(getMinX());
 		if (getX() <= getMinX())
 			setX(getMaxX());
 
